c539bdc721c71efe7eeaab0c6dcf8187c7ed0521
GioXdaZ <gio.daz@gmail.com>
2022-05-11 13:21:50 -0400

    Commmit 6: Add Cards for vids.

--- src/components/BodyContent.jsx
+++ src/components/BodyContent.jsx
@@ -1,10 +1,26 @@
 import "./BodyContent.css"
+import { CardVid } from "./CardVid"
 
 export const BodyContent = () => {
   return (
     <div className="body-background">
       <div className="cards-vids">
-        <h1>DEMO</h1>
+      <CardVid />
+      <CardVid />
+      <CardVid />
+      <CardVid />
+      <CardVid />
+      <CardVid />
+      <CardVid />
+      <CardVid />
+      <CardVid />
+      <CardVid />
+      <CardVid />
+      <CardVid />
+      <CardVid />
+      <CardVid />
+      <CardVid />
+      <CardVid />  
       </div>
     </div>
   )
